7489f66d79e62a363401a2fab63d70062c43735f
mplaty <contact@mplaty.com>
2025-04-05 04:22:51 +1100

    Add beta tag.

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -17,6 +17,7 @@
     </div>
 
     <div id="header-div-button-items" class="header-div-item">
+        <a id="beta-tag" href="https://github.com/mplatypus/platy-paste-frontend?tab=readme-ov-file#beta"><p>beta</p></a>
         <button class="header-div-button-item" onclick={new_paste}>new</button>
     </div>
 </div>
@@ -58,11 +59,31 @@
 
     /* header-div-button-items */
 
+    #header-div-button-items {
+        pad: 2rem;
+    }
+
     .header-div-button-item {
         margin: 0 0.5rem;
         padding: 0.25rem 1rem;
         border-radius: var(--radius-md);
         border-style: solid;
         background-color: var(--color-rose-400);
+    }
+
+    #beta-tag {
+        border-radius: var(--radius-md);
+        border-style: solid;
+        border-width: 0.25rem;
+        border-color: var(--color-yellow-500);
+    }
+
+    #beta-tag > p {
+        padding: 0 0.25rem;
+        margin: 0;
+        border: none;
+        font-family: quicksand, sans-serif;
+        color: var(--color-yellow-500);
+        font-weight: 600;
     }
 </style>
