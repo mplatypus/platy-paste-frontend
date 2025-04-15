5c8bca06454328f6f60e94bb43de0843711d257b
mplaty <contact@mplaty.com>
2025-04-15 14:45:42 +1000

    Attempted fix.

--- src/lib/backend.ts
+++ src/lib/backend.ts
@@ -104,47 +104,52 @@ async function decodeFormData(response: Response): Promise<Paste | null> {
         throw new PasteError(`Unknown Content-Type: ${contentType}`)
     }
     
-    let formData = await response.formData();
+    const getFormData = response.formData.bind(response);
+    const formData = await getFormData();
 
-    let payload_data = formData.get("payload");
+    // Work with the File's `text` method bound explicitly.
+    const getFileText = (file: File) => file.text.bind(file)();
 
-    if (payload_data == null || !(payload_data instanceof File)) {
+    console.log(formData.entries().toArray())
+
+    const payload_data = formData.get("payload");
+    let payloadText: string;
+    if (typeof payload_data === "string") {
+        // Already a JSON string
+        payloadText = payload_data;
+    } else if (payload_data instanceof File) {
+        // Read file contents as text
+        payloadText = await payload_data.text();
+    } else {
         throw new PasteError("Missing payload object.");
     }
-
-    let payload: ResponsePaste = JSON.parse(await payload_data.text())
+    
+    let payload: ResponsePaste = JSON.parse(payloadText);
 
     let documents: Document[] = [];
     for (const document of payload.documents) {
-        let content_data = formData.get(document.id.toString())
-
+        let content_data = formData.get(document.id.toString());
         if (content_data == null) {
-            throw new PasteError(`Content for ${document.id} was not found.`)
+            throw new PasteError(`Content for ${document.id} was not found.`);
         }
-
-        let content: string | null = null
+        let content: string | null = null;
         if (content_data instanceof File) {
-            content = await content_data.text();
+            content = await getFileText(content_data);
         } else {
-            throw new PasteError("Content type is unknown.")
+            throw new PasteError("Content type is unknown.");
         }
-
-        let new_document: Document = {
+        documents.push({
             id: document.id,
             paste_id: document.paste_id,
             type: document.type,
             name: document.name,
             content: content,
-        }
-
-        documents.push(new_document)
+        });
     }
-
     let paste: Paste = {
         id: payload.id,
         edited: payload.edited,
         documents: documents
     }
-
-    return paste
+    return paste;
 }
